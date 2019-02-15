f2e53cb32092da6ec5c511d6b4b6207cc55a879c
Hana Awad <hanaawad_92@hotmail.com>
2019-02-15 18:59:24 +0100

    added photo column to cocktails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,17 +9,4 @@
 
 console.log('Hello World from Webpacker')
 import 'bootstrap';
-
-$(document).ready(function() {
-
-  // Transition effect for navbar
-  $(window).scroll(function() {
-
-    // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 480) {
-        $('.navbar').addClass('solid');
-    } else {
-        $('.navbar').removeClass('solid');
-    }
-  });
-});
+import './nav.js';
---
+++ app/javascript/packs/nav.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navbar-wagon');
+console.log("hello nav bar")
+
+const video = document.querySelector('.video-banner');
+
+if (video) {
+ ;
+}
+
